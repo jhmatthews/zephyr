84809dd945a308be1baf193e9fddbdf6ba7b9d04
jhmatthews <jm8g08@soton.ac.uk>
2017-10-31 00:15:36 +0000

    trying to get code working

--- sv_sub.py
+++ sv_sub.py
@@ -14,7 +14,6 @@ import matplotlib.pyplot as plt
 import scipy.integrate 
 import scipy.optimize
 from collections import namedtuple
-import geo
 import py_read_output as r 
 
 RADIAN=57.29598
@@ -26,109 +25,121 @@ EPSILON=1e-6
 PI=3.1416
 RHO2NH=4.217851e23
 
-def sv_velocity (x):
-	v=np.zeros(3)
-	zzz = v_escape = -99.
-	rzero = sv_find_wind_rzero (x)
-	theta = sv_theta_wind (rzero)
-	r = np.sqrt(x[0] * x[0] + x[1] * x[1])
-	ldist = np.sqrt ((r - rzero) * (r - rzero) + x[2] * x[2])
-	vl = geo.sv_v_zero
-	if (ldist > 0):
-		zzz = pow (ldist / geo.sv_r_scale, geo.sv_alpha)
-		if (rzero < geo.rstar):
-			v_escape = np.sqrt (2. * G * geo.mstar / geo.rstar);
+class sv_wind:
+	def __init__(self, my_object):
+		self.object = my_object
+
+	def get_mdot_norm(self):
+		self.mdot_norm = scipy.integrate.romberg (self.sv_wind_mdot_integral, self.rmin, self.rmax, rtol=1e-6)
+
+	def sv_velocity (self, x):
+		'''
+		Get the wind velocity at a given position vector x 
+		'''
+		v = np.zeros(3)
+		zzz = v_escape = -99.
+		rzero = self.sv_find_wind_rzero (x)
+		theta = self.sv_theta_wind (rzero)
+		r = np.sqrt(x[0] * x[0] + x[1] * x[1])
+		ldist = np.sqrt ((r - rzero) * (r - rzero) + x[2] * x[2])
+		vl = self.v_zero
+		if (ldist > 0):
+			zzz = pow (ldist / self.r_scale, self.alpha)
+			if (rzero < self.object.rstar):
+				v_escape = np.sqrt (2. * G * self.object.mstar / self.object.rstar);
+			else:
+				v_escape = np.sqrt (2. * G * self.object.mstar / rzero);
+			vl = self.v_zero + (self.v_infinity * v_escape - self.v_zero) * zzz / (1. + zzz)
+		
+		v[0] = vl * np.sin (theta)
+		if (r > 0):
+			v[1] = np.sqrt (G * self.object.mstar * rzero) / r
 		else:
-			v_escape = np.sqrt (2. * G * geo.mstar / rzero);
-		vl =geo.sv_v_zero + (geo.sv_v_infinity * v_escape -geo.sv_v_zero) * zzz / (1. + zzz)
-	
-	v[0] = vl * np.sin (theta)
-	if (r > 0):
-		v[1] = np.sqrt (G * geo.mstar * rzero) / r
-	else:
-		v[1] = 0
+			v[1] = 0
 
-	v[2] = vl * np.cos (theta)
+		v[2] = vl * np.cos (theta)
 
-	if (x[2] < 0):		
-		v[2] *= (-1)
-	if (x[1] != 0.0):
-		project_from_cyl_xyz (x, v, xtest)
-		v=stuff_v (xtest)
-	speed = (np.sqrt (v[0] * v[0] + v[1] * v[1] + v[2] * v[2]))
-	return (speed,v)
+		if (x[2] < 0):		
+			v[2] *= (-1)
+
+		if (x[1] != 0.0):
+			self.project_from_cyl_xyz (x, v, xtest)
+			v = self.stuff_v (xtest)
+
+		speed = (np.sqrt (v[0] * v[0] + v[1] * v[1] + v[2] * v[2]))
+
+		return (speed, v)
 
 
 
-def sv_rho (x):
+	def sv_rho (self, x):
 
-	speed,v=sv_velocity (x)
-	rzero = sv_find_wind_rzero (x)
-	theta = sv_theta_wind (rzero)
-	r = np.sqrt (x[0] * x[0] + x[1] * x[1])
-	ldist = np.sqrt ((r - rzero) * (r - rzero) + x[2] * x[2])
-	dmdot_da =geo.wind_mdot * (rzero**(geo.sv_lambda)) * np.cos (theta) / geo.mdot_norm / 2.
-	if (rzero > geo.sv_rmax):
-		rzero = geo.sv_rmax;
-	dtheta_drzero =(sv_theta_wind (rzero)-sv_theta_wind ((1. - EPSILON) * rzero)) / (EPSILON * rzero)
-	dr_drzero = 1. + ldist * dtheta_drzero / np.cos (theta)
+		speed,v = self.sv_velocity (x)
+		rzero = self.sv_find_wind_rzero (x)
+		theta = self.sv_theta_wind (rzero)
+		r = np.sqrt (x[0] * x[0] + x[1] * x[1])
+		ldist = np.sqrt ((r - rzero) * (r - rzero) + x[2] * x[2])
+		dmdot_da = self.wind_mdot * (rzero**(self.sv_lambda)) * np.cos (theta) / self.mdot_norm / 2.
+		if (rzero > self.rmax):
+			rzero = self.rmax;
+		dtheta_drzero =(self.sv_theta_wind (rzero) - self.sv_theta_wind ((1. - EPSILON) * rzero)) / (EPSILON * rzero)
+		dr_drzero = 1. + ldist * dtheta_drzero / np.cos (theta)
 
-	rho = rzero * dmdot_da / (dr_drzero * r * v[2])
-	return (rho)
+		rho = rzero * dmdot_da / (dr_drzero * r * v[2])
+		return (rho)
 
 
 
-def sv_find_wind_rzero (p):
+	def sv_find_wind_rzero (self, p):
 
-	z = np.fabs (p[2])
-	if (z == 0):
-		x = (np.sqrt (p[0] * p[0] + p[1] * p[1]))
+		z = np.fabs (p[2])
+		if (z == 0):
+			x = (np.sqrt (p[0] * p[0] + p[1] * p[1]))
+			return (x)
+		zero_p = self.sv_zero_init (p)	
+		rho_min = self.rmin + z * np.tan (self.thetamin)
+		rho_max = self.rmax + z * np.tan (self.thetamax)
+		rho = np.sqrt (p[0] * p[0] + p[1] * p[1])
+		if (rho <= rho_min):
+			x = self.rmin * rho / rho_min
+			return (x)
+		if (rho >= rho_max):
+			x = self.rmax + rho - rho_max
+			return (x)
+		x = scipy.optimize.brentq (self.sv_zero_r, self.rmin, self.rmax, args=zero_p, xtol=100.)
 		return (x)
-	zero_p=sv_zero_init (p)	
-	rho_min = geo.sv_rmin + z * np.tan (geo.sv_thetamin)
-	rho_max = geo.sv_rmax + z * np.tan (geo.sv_thetamax)
-	rho = np.sqrt (p[0] * p[0] + p[1] * p[1])
-	if (rho <= rho_min):
-		x = geo.sv_rmin * rho / rho_min
+
+
+
+	def sv_zero_init (self, p):
+		zero_p= self.stuff_v (p)
+		zero_p[2] = np.fabs (zero_p[2])
+		return (zero_p)
+
+
+	def stuff_v (self, vin):
+		vout=np.zeros(3)
+		vout[0] = vin[0];
+		vout[1] = vin[1];
+		vout[2] = vin[2];
+		return (vout)
+
+	def sv_theta_wind (self, r):
+		if (r <= self.rmin):
+			return (np.arctan (np.tan (self.thetamin * r / self.rmin)));
+		if (r >= self.rmax):
+			return (self.thetamax);
+		theta = self.thetamin + (self.thetamax - self.thetamin) *  ((r - self.rmin) / (self.rmax - self.rmin)**self.gamma)
+		return (theta);
+
+	def sv_wind_mdot_integral (self, r):
+		x = 2 * PI *  ( r**( self.sv_lambda + 1.)) * np.cos (self.sv_theta_wind (r))
 		return (x)
-	if (rho >= rho_max):
-		x = geo.sv_rmax + rho - rho_max
-		return (x)
-	x = scipy.optimize.brentq (sv_zero_r, geo.sv_rmin, geo.sv_rmax, args=zero_p,xtol=100.)
-	return (x)
 
-
-
-def sv_zero_init (p):
-	zero_p= stuff_v (p)
-	zero_p[2] = np.fabs (zero_p[2])
-	return (zero_p)
-
-
-def stuff_v (vin):
-	vout=np.zeros(3)
-	vout[0] = vin[0];
-	vout[1] = vin[1];
-	vout[2] = vin[2];
-	return (vout)
-
-def sv_theta_wind (r):
-	if (r <= geo.sv_rmin):
-		return (np.arctan (np.tan (geo.sv_thetamin * r / geo.sv_rmin)));
-	if (r >= geo.sv_rmax):
-		return (geo.sv_thetamax);
-	theta = geo.sv_thetamin + (geo.sv_thetamax - geo.sv_thetamin) *  ((r - geo.sv_rmin) / (geo.sv_rmax - geo.sv_rmin)**geo.sv_gamma)
-	return (theta);
-
-
-def sv_wind_mdot_integral (r):
-	x = 2 * PI *  (r**( geo.sv_lambda + 1.)) * np.cos (sv_theta_wind (r))
-	return (x)
-
-def sv_zero_r (r,zero_p):  
-	theta = sv_theta_wind (r);
-	rho = np.sqrt (zero_p[0] * zero_p[0] + zero_p[1] * zero_p[1])
-	rho_guess = r + np.tan (theta) * zero_p[2]
-	return (rho_guess - rho)
+	def sv_zero_r (self, r, zero_p):  
+		theta = self.sv_theta_wind (r);
+		rho = np.sqrt (zero_p[0] * zero_p[0] + zero_p[1] * zero_p[1])
+		rho_guess = r + np.tan (theta) * zero_p[2]
+		return (rho_guess - rho)
 
 
--- zephyr.py
+++ zephyr.py
@@ -1,6 +1,22 @@
 import py_read_output as r 
-import sv_sub as sv
+import sv_sub
+import sys
 import geo
+import numpy as np 
+
+RADIAN=57.29598
+C=2.997925e10
+MSOL=1.979e33
+G=6.670e-8
+YR=3.1556925e7
+EPSILON=1e-6
+PI=3.1416
+RHO2NH=4.217851e23
+
+class compact_object:
+	def __init__(self, mass, radius):
+		self.mstar = mass
+		self.rstar = radius
 
 class grid():
 	
@@ -15,23 +31,42 @@ class grid():
 
 		# read a root filename 
 		self.params = r.read_pf(root)
-		self.sv_thetamin = self.params["sv.diskmin"]
-		self.sv_thetamax = self.params["sv.diskmax"]
-		self.sv_rmin = float(self.params["sv.thetamin"]) / RADIAN
-		self.sv_rmax = float(self.params["sv.thetamax"]) / RADIAN
-		self.wind_rmax = float(self.params["wind.radmax"])
+
+		# get the compact object parameters
+		mass = float(self.params["mstar"]) * MSOL
+		rstar = float(self.params["rstar"]) 
+		self.compact_object = compact_object(mass, rstar)
+
+		# initialise a Shlosman and Vitello wind model
+		self.sv = sv_sub.sv_wind(self.compact_object)
+
+		# set the actual SV parameters from the file 
+		self.sv.r_scale = self.params["sv.acceleration_length"]
+		self.sv.alpha = self.params["sv.acceleration_exponent"]
+		self.sv.rmin = self.params["sv.diskmin"] * self.compact_object.rstar
+		self.sv.rmax = self.params["sv.diskmax"] * self.compact_object.rstar
+		self.sv.thetamin = float(self.params["sv.thetamin"]) / RADIAN
+		self.sv.thetamax = float(self.params["sv.thetamax"]) / RADIAN
+		self.sv.wind_rmax = float(self.params["wind.radmax"])
+		self.sv.v_zero = 6e5 		# hardwired value of 6 km/s
+		self.sv.v_infinity = float(self.params["wind.radmax"]) 		# hardwired value of 6 km/s
+		self.sv.sv_lambda = float(self.params["sv.mdot_r_exponent"])
+		self.sv.gamma = 1.0
+		self.sv.wind_mdot = float(self.params["wind.mdot"]) * MSOL / YR
+
+		self.sv.get_mdot_norm()
+
+
 
 	def setup(self):
 
-		self.nx = self.params["Wind.dim.in.x_or_r.direction"]
-		self.nz = self.params["Wind.dim.in.z_or_theta.direction"]
+		self.nx = int(self.params["Wind.dim.in.x_or_r.direction"])
+		self.nz = int(self.params["Wind.dim.in.z_or_theta.direction"])
 
 		# hardwired x and z scales at the moment, as well as "cen_or_not"
-		self.x, self.z = geo.get_grid(self.rstar/10.0, self.sv_rmin, nz, nx, self.rmax, 0)
+		self.x, self.z = geo.get_grid(self.compact_object.rstar/10.0, self.sv.rmin, self.nz, self.nx, self.sv.wind_rmax, 0)
 
-		self.density = np.empty([len(x),len(z)])
-		self.nh = np.empty([len(x),len(z)])
-
+		print (self.z)
 
 	def populate_primitive_variables(self):
 		'''
@@ -39,23 +74,23 @@ class grid():
 
 		IMPROVE: make more pythonic, less for looping please!
 		'''
-		self.nh = np.zeros([len(x), len(z)])
+		self.nh = np.zeros([len(self.x), len(self.z)])
 		self.density = np.zeros_like(self.nh)
-		self.velocity = np.zeros(self.nh)
+		self.velocity = np.zeros_like(self.nh)
 
 		for  j in range(len(self.z)):
 			for i in range(len(self.x)):
-				rho_min = self.sv_rmin + z[j] * np.tan (self.sv_thetamin)
-				rho_max = self.sv_rmax + z[j] * np.tan (self.sv_thetamax)
+				rho_min = self.sv.rmin + self.z[j] * np.tan (self.sv.thetamin)
+				rho_max = self.sv.rmax + self.z[j] * np.tan (self.sv.thetamax)
 
-				if (x[i] <= rho_max and x[i] >= rho_min):
+				if (self.x[i] <= rho_max and self.x[i] >= rho_min):
 
 					# 3d vector, x, theta, z 
-					xtest = [x[i],0.0,z[j]]
+					xtest = [self.x[i], 0.0, self.z[j]]
 
 					# get density from sv subroutines, convert to hydrogen density 
-					self.density[i,j] = sv.sv_rho(xtest)
-					n_h = density[i,j] * sv.RHO2NH
+					self.density[i,j] = self.sv.sv_rho(xtest)
+					n_h = self.density[i,j] * RHO2NH
 
 					# copy to array
 					self.nh[i,j] = np.log10(n_h)
@@ -70,13 +105,19 @@ class grid():
 
 if __name__ == "__main__":
 
+	root = sys.argv[1]
+
 	mod = grid(root)
 	mod.setup()
 	mod.populate_primitive_variables()
 
 	import matplotlib.pyplot as plt 
-	plt.pcolormesh(mod.x, mod.z, mod.nh)
+
+	print (mod.x, mod.z, mod.nh)
+	plt.pcolormesh(mod.x, mod.z, mod.nh.T, vmin = 4, vmax=14)
 	plt.loglog()
+	plt.xlim(1e15,1e20)
+	plt.ylim(1e15,1e20)
 	plt.show()
 
 
